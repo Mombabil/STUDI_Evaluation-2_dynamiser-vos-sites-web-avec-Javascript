3e882fc7af3a9aecef72fb48782317a119ce6d4a
Morgan Perret <morgan.perret.7@gmail.com>
2021-12-16 12:55:36 +0100

    Edit#11 - Creation fonction changement de joueur et barre de vie descendante

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function choicePlayer1 (){
+//choix des joueurs, en selectionnant un joueur, la barre de vie de chaques joueurs s'initialise a 100
+function choicePlayer(){
+    // choix joueur 1
     let player1 = document.getElementById('select-player-1')
     let imgPlayer1 = document.getElementById('imgPlayer1')
 
+    //On choisi le joueur et on affiche son stuff, une fois le choix fait, 
+    //la liste devient disabled et le joueur ne peut plus changer de personnage
     player1.addEventListener('change', (action1) => {
         switch(action1.target.value){
             case 'Orayon-1':
@@ -31,11 +35,12 @@ function choicePlayer1 (){
         }
         lifeBar1()
     })
-}
-function choicePlayer2 (){
+    // choix joueur 2
     let player2 = document.getElementById('select-player-2')
     let imgPlayer2 = document.getElementById('imgPlayer2')
 
+    //On choisi le joueur et on affiche son stuff, une fois le choix fait, 
+    //la liste devient disabled et le joueur ne peut plus changer de personnage
     player2.addEventListener('change', (action2) => {
         switch(action2.target.value){
             case 'Orayon-2':
@@ -66,43 +71,56 @@ function choicePlayer2 (){
         lifeBar2()
     })
 }
-function lifeBar1 (){
-    let canvasP1 = document.getElementById('lifePlayer1')
-    let ctxP1
-
-    if(canvasP1.getContext){
-        ctxP1 = canvasP1.getContext('2d')
-    }else{
-        alert('Votre navigateur est trop ancien et ne peut pas afficher le code Javascript, merci de mettre a jour votre navigateur')
-    }
-    ctxP1.fillStyle = 'green'
-    ctxP1.fillRect(0,0, 440, 43)
-
-
+//Lorsque l'utilisateur a choisi son joueur, la barre de vie de son personnage se remplit a 100%
+//barre de vie joueur 1
+function lifeBar1(){
+    let life = document.getElementById('lifePlayer1')
+    life.value = 100
 }
 function lifeBar2(){
-    let canvasP2 = document.getElementById('lifePlayer2')
-    let ctxP2
-
-    if(canvasP2.getContext){
-        ctxP2 = canvasP2.getContext('2d')
-    }else{
-        alert('Votre navigateur est trop ancien et ne peut pas afficher le code Javascript, merci de mettre a jour votre navigateur')
-    }
-    ctxP2.fillStyle = 'green'
-    ctxP2.fillRect(0,0, 440, 43)
+    let life = document.getElementById('lifePlayer2')
+    life.value = 100
 }
-function loadPlayer1(){
-    let load = document.getElementById('loadPlayer1')
+
+//Le dé change de face lorsque l'on clique sur charger et la valeur augmente tant que le dé ne vaut pas 1
+//Si le dé vaut 1, on change de joueur
+//Si le joueur clique sur attaquer, le cumul de ses lancé est déduit des points de vie de l'adversaire
+function loadPlayer(){
+    let load1 = document.getElementById('loadPlayer1')
+    let attack1 = document.getElementById('attackPlayer1')
     let de = document.getElementById('de')
-    let loadEnnemy = document.getElementById('loadPlayer2')
-    let attackEnnemy = document.getElementById('attackPlayer2')
+    let load2 = document.getElementById('loadPlayer2')
+    let attack2 = document.getElementById('attackPlayer2')
+    let puissance1 = document.getElementById('puissance1')
+    let puissance2 = document.getElementById('puissance2')
+    let count = 0
 
-    loadEnnemy.disabled = true
-    attackEnnemy.disabled = true
+    let imgPlayer1 = document.getElementById('imgPlayer1')
+    let imgPlayer2 = document.getElementById('imgPlayer2')
 
-    load.addEventListener('click', (action) =>{
+
+    load2.disabled = true
+    attack2.disabled = true
+
+    //ACTIONS SUR LES BOUTONS CHARGER
+    // lancement du dé joueur 1
+    load1.addEventListener('click', (action) =>{
+        
+        //on envoie une alerte si aucun joueur n'est selectionné
+        let player1 = document.getElementById('select-player-1')
+        if(player1.value ===''){
+            alert('Selectionner d\'abord un joueur')
+            return
+        }
+        //comme le joueur un attaque en premier, il faut mettre l'alerte pour les 2 joueurs
+        let player2 = document.getElementById('select-player-2')
+        if(player2.value ===''){
+            alert('Selectionner d\'abord un joueur')
+            return
+        }
+
         let random = Math.floor(Math.random() * (6 - 0)) +1 
+        //selon le resultat, on affiche la face du dé correspondant
         switch(random){
             case 1:
                 de.src='images/de1.jpg'
@@ -123,19 +141,31 @@ function loadPlayer1(){
                 de.src='images/de6.jpg'
                 break
         }
+        count = count + random
+        //on incrémente les valeurs de chaques clics sur le bouton CHARGER et on les affichent
+        if(random === 1){
+            count = 0
+            load1.disabled = true
+            attack1.disabled = true
+            load2.disabled = false
+            attack2.disabled = false
+            random = 0
+
+        }
+        puissance1.innerText = count
     })
-}
-function loadPlayer2(){
-    let load = document.getElementById('loadPlayer2')
-    let de = document.getElementById('de')
-    let loadEnnemy = document.getElementById('loadPlayer1')
-    let attackEnnemy = document.getElementById('attackPlayer1')
+    // lancement du dé joueur 2
+    load2.addEventListener('click', (action) =>{
 
-    loadEnnemy.disabled = true
-    attackEnnemy.disabled = true
+        //on envoie une alerte si aucun joueur n'est selectionné
+        let player2 = document.getElementById('select-player-2')
+        if(player2.value ===''){
+            alert('Selectionner d\'abord un joueur')
+            return
+        }
 
-    load.addEventListener('click', (action) =>{
         let random = Math.floor(Math.random() * (6 - 0)) +1 
+        //selon le resultat, on affiche la face du dé correspondant
         switch(random){
             case 1:
                 de.src='images/de1.jpg'
@@ -156,13 +186,72 @@ function loadPlayer2(){
                 de.src='images/de6.jpg'
                 break
         }
+        count = count + random
+        //on incrémente les valeurs de chaques clics sur le bouton CHARGER et on les affichent
+        if(random === 1){
+            count = 0
+            load2.disabled = true
+            attack2.disabled = true
+            load1.disabled = false
+            attack1.disabled = false
+
+        }
+        puissance2.innerText = count
+    })
+
+    //ACTIONS SUR LES BOUTON ATTAQUER
+    //attaque du joueur 1
+    let remainingLifeP2 = 100
+    attack1.addEventListener('click', (action) =>{
+
+        //on envoie une alerte si aucun joueur n'est selectionné
+        let player1 = document.getElementById('select-player-1')
+        if(player1.value ===''){
+            alert('Selectionner d\'abord un joueur')
+            return
+        }
+        //comme le joueur un attaque en premier, il faut mettre l'alerte pour les 2 joueurs
+        let player2 = document.getElementById('select-player-2')
+        if(player2.value ===''){
+            alert('Selectionner d\'abord un joueur')
+            return
+        }
+
+        let life = document.getElementById('lifePlayer2')
+        remainingLifeP2 = remainingLifeP2 - puissance1.innerText
+        life.value = remainingLifeP2
+        puissance1.innerText = 0
+        count = 0
+        load2.disabled = false
+        attack2.disabled = false
+        load1.disabled = true
+        attack1.disabled = true
+
+    })
+    //attaque du joueur 2
+    let remainingLifeP1 = 100
+    attack2.addEventListener('click', (action) =>{
+
+        //on envoie une alerte si aucun joueur n'est selectionné
+        let player2 = document.getElementById('select-player-2')
+        if(player2.value ===''){
+            alert('Selectionner d\'abord un joueur')
+            return
+        }
+
+        let life = document.getElementById('lifePlayer1')
+        remainingLifeP1 = remainingLifeP1 - puissance2.innerText
+        life.value = remainingLifeP1
+        puissance2.innerText = 0
+        count = 0
+        load1.disabled = false
+        attack1.disabled = false
+        load2.disabled = true
+        attack2.disabled = true
+
     })
 }
 
-//Si le joueur n'a pas initialisé la partie, en selectionnant les deux joueurs, un message d'erreur apparait
-function error(){
-    alert('Vous devez d\'abord choisir les joueurs avant de lancer les dés')
-}
-choicePlayer1()
-choicePlayer2()
-loadPlayer2()
+choicePlayer()
+loadPlayer()
+
